3f173175084493313c70fad62fff1195d77f0edd
LnkAsia <120370945+LnkAsia@users.noreply.github.com>
2025-04-07 18:07:03 +0530

    Update OWNCLOUD.cmake

--- OWNCLOUD.cmake
+++ OWNCLOUD.cmake
@@ -4,7 +4,7 @@ set( APPLICATION_EXECUTABLE "picloud" )
 set( APPLICATION_DOMAIN     "owncloud.com" )
 set( APPLICATION_VENDOR     "piCloud" )
 set( APPLICATION_UPDATE_URL "https://updates.owncloud.com/client/" CACHE STRING "URL for updater" )
-set( APPLICATION_ICON_NAME  "ownloud" )
+set( APPLICATION_ICON_NAME  "owncloud" )
 set( APPLICATION_VIRTUALFILE_SUFFIX "owncloud" CACHE STRING "Virtual file suffix (not including the .)")
 
 set( LINUX_PACKAGE_SHORTNAME "owncloud" )
